2ae106ddcbc22cfa4d1827662c65525765ac4040
ArktinenSieni <matti.raty@helsinki.fi>
2015-01-19 15:59:56 +0200

    Perusrakenne, Hahmo-luokka

--- TheChase/src/thechase/logiikka/asiat/Hahmo.java
+++ TheChase/src/thechase/logiikka/asiat/Hahmo.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package thechase;
+package thechase.logiikka.asiat;
 
 /**
  *
  * @author TheArctic
  */
-public class TheChase {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Hahmo {
     
 }
